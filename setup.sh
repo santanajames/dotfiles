6efedd0856a86243f2063932cd12338b6f3b6f25
Santana James <santanajames@users.noreply.github.com>
2024-02-29 17:58:28 -0500

    add antidote plugins

--- setup.sh
+++ setup.sh
@@ -1,17 +1,66 @@
 #!/bin/zsh
+- install mac cli tools
+- install homebrew
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+```
 
+- add homebrew to shell
+```
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ec938378/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+```
+- brew install chezmoi
+- chezmoi init santanajames --apply
+- volta install node
+
+ssh-keygen -t ed25519 -C "santana.james@gmail.com"
+eval "$(ssh-agent -s)"
+code ~/.ssh/config
+```
+Host github.com
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/id_ed25519
+```
+ssh-add ~/.ssh/id_ed25519
+pbcopy < ~/.ssh/id_ed25519.pub
+
+# ---------------
 # MacOS
 # ---------------
+
 # Core
+# =====
+# Turn off startup sound
 sudo nvram SystemAudioVolume=" "
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
 # Finder
+# =====
+# Disable screenshot shadow when capturing an app
+defaults write com.apple.screencapture disable-shadow -bool true
+# Show all file extensions inside the Finder
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-defaults write com.apple.finder AppleShowAllFiles YES
+# Show hidden files inside the Finder
+defaults write com.apple.finder AppleShowAllFiles -bool true
+# Show path bar
 defaults write com.apple.finder ShowPathbar -bool true
+# Search the current folder
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+# Remove the delay when hovering the toolbar title
+defaults write NSGlobalDomain NSToolbarTitleViewRolloverDelay -float 0
+# Show the Finder status bar
 defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.dock mru-spaces -bool false
+
+# Menu Bar
+# Show battery Percentage
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+# Mission Control
+# =====
+# Do not reorder Spaces based on most recent use
+defaults write com.apple.dock "mru-spaces" -bool false
+
+killall Finder
 
 # Git 
 # ---------------
